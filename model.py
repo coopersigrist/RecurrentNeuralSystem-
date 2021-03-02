ae283959d583eec3cc642998d124fc43066fd2db
coopersigrist <csigrist@umass.edu>
2021-03-02 14:14:42 -0500

    refactored code

---
+++ model.py
@@ -0,0 +1,267 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from tqdm import tqdm
+from typing import Optional, Union, Tuple, List, Sequence, Iterable
+import math
+from scipy.spatial.distance import euclidean
+from torch.nn.modules.utils import _pair
+from torchvision import models
+from sklearn.metrics import jaccard_score
+import matplotlib.pyplot as plt
+
+class recurrentLayer(nn.Module):
+
+    def __init__(self, in_size, out_size, next_size, previous_size, reflexor_size, cdu_thresh, depth=1):
+
+        print("new layer")
+
+        self.in_size = in_size
+        self.out_size = out_size
+        self.next_size = next_size
+        self.previous_size = previous_size
+        self.reflexor_size = reflexor_size
+        self.cdu_thresh = cdu_thresh
+        self.depth = depth
+
+        
+
+        super().__init__()
+
+        self.soft = torch.nn.Softmax(dim=1)
+
+        if(self.in_size <= self.reflexor_size or depth > 3):
+            self.trivial = True
+            self.simple_layer = nn.Linear(self.in_size, self.out_size)
+        else:
+            self.trivial = False
+            self.next = recurrentLayer(in_size=self.next_size, out_size=reflexor_size, next_size=math.floor(self.next_size/2), previous_size=math.floor(self.out_size/2), reflexor_size=math.ceil(self.reflexor_size * 2), cdu_thresh=self.cdu_thresh, depth=self.depth+1)   
+            self.previous = recurrentLayer(in_size=self.reflexor_size, next_size=math.floor(self.reflexor_size * 2), previous_size=math.floor(self.previous_size/2), out_size=self.previous_size, reflexor_size=math.floor(self.reflexor_size * 2), cdu_thresh=self.cdu_thresh, depth=self.depth+1)
+            self.in_layer = nn.Linear(self.in_size, self.next_size, True)
+            self.out_layer = nn.Linear(self.previous_size, self.out_size, True)
+
+
+    def forward(self, x):
+        if(self.trivial):
+            return self.simple_layer(x)
+
+        out = self.in_layer(x)
+        out = torch.sigmoid(out)
+        out = self.next.forward(out)
+        out = torch.sigmoid(out)
+        out = self.previous.forward(out)
+        out = torch.sigmoid(out)
+        out = self.out_layer(out)
+        out = self.soft(out)
+
+        return out
+
+class RegularAutoEncoder(nn.Module):
+
+    def __init__(self, in_size, out_size, reflexor_size):
+
+        self.in_size = in_size
+        self.out_size = out_size
+        self.reflexor_size = reflexor_size
+
+        
+
+        super().__init__()
+
+        self.soft = torch.nn.Softmax(dim=1)
+
+        self.fc1 = nn.Linear(in_size, 300)
+        self.fc2 = nn.Linear(300, reflexor_size)
+        self.fc3 = nn.Linear(reflexor_size, 300)
+        self.fc4 = nn.Linear(300, out_size)
+
+
+    def forward(self, x):
+
+
+        out = self.fc1(x.view(-1, self.in_size))
+        out = torch.sigmoid(out)
+        out = self.fc2(out)
+        out = torch.sigmoid(out)
+        out = self.fc3(out)
+        out = torch.sigmoid(out)
+        out = self.fc4(out)
+
+        return out
+
+class Modulator(nn.Module):
+
+    def __init__(self, in_size, conv_size, out_size):
+
+        self.in_size = in_size
+        self.out_size = out_size
+        self.conv_size = conv_size  
+
+        super().__init__()
+
+        self.soft = torch.nn.Softmax(dim=1)
+
+        self.conv = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(10,10), padding=(1,1))
+        self.fc1 = nn.Linear(21*21, out_size)
+
+        self.relu = nn.ReLU()
+
+
+
+    def forward(self, x):
+
+
+        out = self.conv(x).view(-1, 441)
+        self.relu(out)
+        out = self.fc1(out)
+        out = torch.sigmoid(out)
+
+
+        return out
+
+class LinModulator(nn.Module):
+
+    def __init__(self, in_size, out_size):
+
+        self.in_size = in_size
+        self.out_size = out_size 
+
+        super().__init__()
+
+        self.soft = torch.nn.Softmax(dim=1)
+
+        self.fc1 = nn.Linear(in_size, out_size)
+
+
+    def forward(self, x):
+
+        out = self.fc1(x.view(-1, self.in_size))
+        out = torch.sigmoid(out)
+
+
+        return out
+
+class ModulatedAutoEncoder(nn.Module):
+
+    def __init__(self, in_size, out_size, reflexor_size):
+
+        self.in_size = in_size
+        self.out_size = out_size
+        self.reflexor_size = reflexor_size
+
+        
+
+        super().__init__()
+
+        self.soft = torch.nn.Softmax(dim=1)
+
+
+        self.mod = Modulator(in_size, (4,4), reflexor_size)
+        self.fc1 = nn.Linear(in_size, 200)
+        self.fc2 = nn.Linear(200, reflexor_size)
+        self.fc3 = nn.Linear(reflexor_size, 200)
+        self.fc4 = nn.Linear(200, out_size)
+
+
+    def forward(self, x):
+
+        mod = self.mod(x)
+        out = self.fc1(x.view(-1, 784))
+        out = torch.sigmoid(out)
+        out = self.fc2(out)
+        out = torch.sigmoid(out)
+        out = out * mod
+        out = self.fc3(out)
+        out = torch.sigmoid(out)
+        out = self.fc4(out)
+
+        return out
+
+class PseudoRecLayer(nn.Module):
+
+    def __init__(self, in_size, out_size, reflexor_size, modulator):
+
+        self.in_size = in_size
+        self.out_size = out_size
+        self.reflexor_size = reflexor_size
+
+
+
+        
+
+        super().__init__()
+
+        self.fc1 = nn.Linear(in_size, reflexor_size)
+        self.fc2 = nn.Linear(reflexor_size, out_size)
+        self.mod = modulator
+        self.relu = nn.ReLU()
+
+
+
+    def forward(self, x):
+
+        mod = self.mod(x)
+        out = self.fc1(x.view(-1, self.in_size))
+        out = self.relu(out)
+        out = out * mod
+        out = self.fc2(out)
+
+        return out
+
+class PseudoRecAutoEncoder(nn.Module):
+
+    def __init__(self, in_size, out_size, reflexor_size):
+
+        self.in_size = in_size
+        self.out_size = out_size
+        self.reflexor_size = reflexor_size
+
+
+        
+
+        super().__init__()
+
+
+        self.mod1 = LinModulator(in_size, reflexor_size)
+        self.mod2 = LinModulator(in_size//2, reflexor_size//2)
+        self.mod3 = LinModulator(in_size//2, reflexor_size//2)
+
+        print("made modulators")
+
+        self.rec1 = PseudoRecLayer(in_size//2, reflexor_size, reflexor_size//2, self.mod2)
+        self.rec2 = PseudoRecLayer(in_size//2, reflexor_size, reflexor_size//2, self.mod3)
+
+        print("made recurrers")
+
+        self.fc1 = nn.Linear(reflexor_size, 200)
+        self.fc2 = nn.Linear(200, out_size)
+
+        self.relu = nn.ReLU()
+
+
+    def forward(self, x):
+
+        mod1 = self.mod1(x)
+        out = x.view(-1, 784)
+
+        # FIRST RECURENCE LAYER
+        first_half = out[:, 392:]
+        rec1 = self.rec1(first_half)
+
+        # SECOND RECURENCE LAYER
+        second_half = out[:, :392]
+        rec2 = self.rec2(second_half)
+
+        out = rec1 * rec2
+        out = self.relu(out)
+        out = out * mod1
+        out = self.relu(out)
+
+        out = self.fc1(out)
+        out = self.fc2(out)
+
+
+        return out
---
+++ test.py
@@ -0,0 +1,141 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from tqdm import tqdm
+from typing import Optional, Union, Tuple, List, Sequence, Iterable
+import math
+from scipy.spatial.distance import euclidean
+from torch.nn.modules.utils import _pair
+from torchvision import models
+from sklearn.metrics import jaccard_score
+import matplotlib.pyplot as plt
+from model import RegularAutoEncoder, ModulatedAutoEncoder, PseudoRecAutoEncoder
+
+batch_size = 32
+num_epochs = 2
+
+# Load MNIST data.
+train_data = dsets.MNIST(root = './data', train = True,
+                        transform = transforms.ToTensor(), download = True)
+
+test_data = dsets.MNIST(root = './data', train = False,
+                       transform = transforms.ToTensor())
+
+train_gen = torch.utils.data.DataLoader(dataset = train_data,
+                                             batch_size = batch_size,
+                                             shuffle = True)
+
+test_gen = torch.utils.data.DataLoader(dataset = test_data,
+                                      batch_size = batch_size, 
+                                      shuffle = False)
+
+# net = recurrentLayer(784, 784, 10, 5, 10, 0)
+net1 = RegularAutoEncoder(784, 784, reflexor_size)
+net2 = ModulatedAutoEncoder(784, 784, reflexor_size)
+net3 = PseudoRecAutoEncoder(784, 784, reflexor_size)
+
+
+
+lr = .0001 # size of step 
+loss_function = nn.MSELoss()
+
+train_losses = [[],[],[]]
+test_losses = [[],[],[]]
+
+real_imgs = [[],[],[]]
+reconstructed_imgs = [[],[],[]]
+
+param_counts = np.ones(3)
+
+steps = [[],[],[]]
+
+for num, net in enumerate([net1, net2, net3]):
+
+  optimizer = torch.optim.Adam( net.parameters(), lr=lr)
+  param_counts[num] = (sum(p.numel() for p in net.parameters() if p.requires_grad))
+
+  for epoch in range(num_epochs):
+    for i ,(images,labels) in enumerate(train_gen):
+      #images = Variable(images.view(-1,28*28))
+      labels = Variable(images.view(-1,28*28))
+      
+      optimizer.zero_grad()
+      outputs = net(images)
+      loss = loss_function(outputs, labels)
+      loss.backward()
+      optimizer.step()
+      
+      if (i+1) % 300 == 0:
+        temp_loss = loss.item()
+        print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
+                  %(epoch+1, num_epochs, i+1, len(train_data)//batch_size, temp_loss))
+        dupe = Variable(outputs[0].data, requires_grad=False)
+        # plt.imshow(images[0].view(28, 28))
+        # plt.show()
+        # plt.imshow(dupe.view(28, 28))
+        # plt.show()
+        train_losses[num].append(temp_loss)
+        steps[num].append((50000 * epoch) + ((i + 1) * batch_size))
+
+        real_imgs[num].append(images[0].view(28, 28))
+        reconstructed_imgs[num].append(dupe.view(28, 28))
+    
+        # Test Data
+
+        score = 0
+        total = 0
+        for images,labels in test_gen:
+          #images = Variable(images.view(-1,784))
+          
+          output = net(images)
+          score += loss_function(output, images.view(-1, 784))
+        test_losses[num].append((score))
+
+then plt.plot(steps[0], train_losses[0], label= "Baseline")
+plt.plot(steps[1], train_losses[1], label= "Modulated")
+plt.plot(steps[2], train_losses[2], label= "Recurrent with Modulation")
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
+plt.title('Training loss history')
+plt.legend()
+plt.show()
+
+plt.plot(steps[0], test_losses[0], label= "Baseline")
+plt.plot(steps[1], test_losses[1], label= "Modulated")
+plt.plot(steps[2], test_losses[2], label= "Recurrent with Modulation")
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
+plt.title('Testing loss history')
+plt.legend()
+plt.show()
+
+for num,count in enumerate(param_counts):
+  param_counts[num] /= 1000
+
+
+plt.bar(["Base", "Modulated", "ReNS"], param_counts)
+plt.xlabel('Model')
+plt.ylabel('# of thousands of Parameters')
+plt.show()
+
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+num_smaples = len(real_imgs[0])
+
+
+for num in [0,1,2]:
+  fig = plt.figure(figsize=(20.,20.))
+  grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                  nrows_ncols=(2, num_smaples),  # creates 2x2 grid of axes
+                  axes_pad=0.1,  # pad between axes in inch.
+                  )
+
+  for ax, im in zip(grid, real_imgs[num]+reconstructed_imgs[num]):
+      # Iterating over the grid returns the Axes.
+      ax.imshow(im)
+      ax.axis("off")
+
+  plt.show()
